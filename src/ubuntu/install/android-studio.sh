079ab781af9077d8c56e75baecb7c89da2b4293b
Your Name <you@example.com>
2019-01-19 23:42:15 +0700

    fix

--- src/ubuntu/install/android-studio.sh
+++ src/ubuntu/install/android-studio.sh
@@ -5,7 +5,12 @@ set -u
 
 echo "Install Android Studio"
 apt-get update 
-apt-get install -y snap
+apt-get install -y lib32stdc++6 unzip
 apt-get clean -y
-# via snap
-sudo snap install android-studio --classic
+
+wget -qO- https://dl.google.com/dl/android/studio/ide-zips/3.3.0.20/android-studio-ide-182.5199772-linux.zip
+unzip android-studio-ide-182.5199772-linux.zip
+rm -fr android-studio-ide-182.5199772-linux.zip
+mv android-studio /opt/
+chmod +x /opt/android-studio/bin/studio.sh
+
