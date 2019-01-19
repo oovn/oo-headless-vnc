d07840dfb10aa57f5437b553a09cc191a6c4c364
Your Name <you@example.com>
2019-01-19 22:52:06 +0700

    fix

--- src/ubuntu/install/android-studio.sh
+++ src/ubuntu/install/android-studio.sh
@@ -2,10 +2,9 @@
 ### every exit != 0 fails the script
 set -e
 
-echo "Install Chromium Browser"
+echo "Install Android Studio"
 apt-get update 
-apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+apt-get install -y snap
 apt-get clean -y
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+# via snap
+snap install android-studio --classic
