4a732576e0a0c7862a98ed5f3b40c3c0aec24e5c
Your Name <you@example.com>
2019-01-22 22:31:14 +0700

    vs

--- src/ubuntu/install/visualstudio.sh
+++ src/ubuntu/install/visualstudio.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
+set -u
 
 echo "Install Visual Studio"
 
-wget -O visualstudio.deb https://go.microsoft.com/fwlink/?LinkID=760868
-apt-get install visualstudio.deb
-rm -fr visualstudio.deb
+wget -O /tmp/visualstudio.deb https://go.microsoft.com/fwlink/?LinkID=760868
+dpkg -i /tmp/visualstudio.deb
+rm -fr /tmp/visualstudio.deb
